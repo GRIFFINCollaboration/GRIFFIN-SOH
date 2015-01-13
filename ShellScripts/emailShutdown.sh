f23daf97abb53a0c01fbf1972327cfe752c96000
grifsoh <grifsoh@grifsoh00.triumf.ca>
2015-01-13 13:16:44 -0800

    Generate e-mail message when crates are powered off. Make things executable. Fix confusing references to tigsoh in e-mails.

---
+++ ShellScripts/emailShutdown.sh
@@ -0,0 +1,1 @@
+echo "GRIFFIN Shack Overheat ALARM! VME Crate power now shut off. See grifsoh00.triumf.ca:8081" | mail -r "$USER@$HOSTNAME" -s "GRIFFIN Shack Overheat ALARM!" equip@grsimail.triumf.ca
